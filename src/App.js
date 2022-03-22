bc50006a6ee01339a613fe9c2b9a8b4d1434528f
dhana-dev-web <100456070+dhana-dev-web@users.noreply.github.com>
2022-03-22 23:47:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
+import LoginForm from "./components/LoginForm";
+import "./App.css";
+
+const projectID = "407d8dbf-8156-4843-8808-bfdce0a2a3be";
+
+const App = () => {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+};
+
+export default App;
